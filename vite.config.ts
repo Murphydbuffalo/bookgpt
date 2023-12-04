a1513ff2d10922fd82d89c9a5639acc10eb7dddf
Dan Murphy <dan_murphy@hey.com>
2023-12-04 14:59:21 -0700

    Install Vite React plugin and render test JSX

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import Rails from 'vite-plugin-rails'
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
   plugins: [
     Rails({
       envVars: { RAILS_ENV: 'development' },
     }),
+    react(),
   ],
 })
