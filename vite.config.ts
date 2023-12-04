275d44085f202f26284e8f2e8821941d962f4e9e
Dan Murphy <dan_murphy@hey.com>
2023-12-04 14:38:40 -0700

    Use vite-plugin-rails

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import Rails from 'vite-plugin-rails'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
+    Rails({
+      envVars: { RAILS_ENV: 'development' },
+    }),
   ],
 })
