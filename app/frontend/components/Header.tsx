9c8efb5afc486dc79cdfd74e66efaf95e16d8e96
Dan Murphy <dan_murphy@hey.com>
2023-12-05 13:38:32 -0700

    Style header

--- app/frontend/components/Header.tsx
+++ app/frontend/components/Header.tsx
@@ -1,11 +1,18 @@
 export default function Header() {
   return (
     <header>
-      <h1>Ask The Mom Test</h1>
-      <p>
-        <a href='https://hailpixel.gumroad.com/l/momtest' target='_blank'>Buy on Gumroad</a>
-      </p>
-      <img src='/vite/assets/momtest-fCbclsqR.webp' />
+      <div className='invisible flex-spacer'></div>
+
+      <div>
+        <h1>Ask The Mom Test</h1>
+        <p>
+          <a href='https://hailpixel.gumroad.com/l/momtest' target='_blank'>Buy on Gumroad</a>
+        </p>
+      </div>
+      
+      <a href='https://hailpixel.gumroad.com/l/momtest' target='_blank'>
+        <img width="200px" src='/vite/assets/momtest-fCbclsqR.webp' />
+      </a>
     </header>
   );
 };
