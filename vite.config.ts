5db68a8fbb76d7003012f1836c4c59017a1e6e72
Dan Murphy <dan_murphy@hey.com>
2023-12-07 10:45:50 -0700

    Revert "Don't hardcode env in vite config"
    This reverts commit f0f137c88141eaf9932770e0bc06d0ea5ebb15fe.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [
     react(),
-    Rails(),
+    Rails({
+      envVars: { RAILS_ENV: 'development' },
+    }),
   ],
 })
